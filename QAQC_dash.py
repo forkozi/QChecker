f1a500f0eda3c424fe1e247fec20d284d7662f52
Nick.Forfinski-Sarko <Nick.Forfinski-Sarko@NGS-W-004085996>
2018-11-06 17:36:24 -0500

    rearranged layout into tabs

--- QAQC_dash.py
+++ QAQC_dash.py
@@ -21,7 +21,9 @@ from csv import DictReader
 
 
 def generate_table(dataframe, max_rows=10):
-    return html.Table(
+    return html.Table(style={
+        'overflow': 'auto',
+        }, children=
         # Header
         [html.Tr([html.Th(col, 
                 style={
@@ -42,7 +44,9 @@ def generate_table(dataframe, max_rows=10):
                     'padding-top':'1px', 
                     'padding-bottom':'1px', 
                     'text-align':'left',
-                    'border-bottom':'blue'})  # TODO: not what I wanted, but I like it! (fix)
+                    'border-bottom':'blue',
+                    'white-space': 'nowrap',                    
+                    })  # TODO: not what I wanted, but I like it! (fix)
             for col in dataframe.columns
         ]) for i in range(min(len(dataframe), max_rows))]
     )
@@ -69,7 +73,7 @@ map_colors = {
     }
 
 
-def get_qaqc_map(check_name, check_label, contract_tile_csv, qaqc_results_csv):
+def get_qaqc_map(check_name, check_label, map_style, contract_tile_csv, qaqc_results_csv):
 	# groupby returns a dictionary mapping the values of the first field 
 	# 'classification' onto a list of record dictionaries with that 
 	# classification value.
@@ -120,7 +124,7 @@ def get_qaqc_map(check_name, check_label, contract_tile_csv, qaqc_results_csv):
 		"data":  qaqc_tiles + contract_tiles,
 		"layout": {
 			'legend': dict(
-					x=1,
+					x=0,
 					y=0,
 					traceorder='normal',
 					font=dict(
@@ -147,11 +151,11 @@ def get_qaqc_map(check_name, check_label, contract_tile_csv, qaqc_results_csv):
                 "bearing": 0,
                 "center": {
                     "lat": 41.35,
-                    "lon": -70.3
+                    "lon": -70.2
                 },
                 "pitch": 0,
                 "zoom": 10,
-                "style": "dark"
+                "style": map_style,
             }
         }
     }
@@ -198,17 +202,23 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 colors = {
 	'background': '#373D42',
 	'text': '#B1C4D2',
-    'header': '#0968AA'
+    'header': '#0968AA',
+    'button_background': '#75777a',
 }
 
 map_button_style = {
     'color': colors['text'],
     'border-color': colors['text'],
-    'background-color': colors['background'],
+    'background-color': colors['button_background'],
     'border-radius': '50px',
     'padding': '0px',
     'line-height': '1px',
 }
+
+map_control_label_style = {
+    'color': colors['text'],
+    'margin-top': '1em',
+    }
 
 checks_to_do = {
 	'naming_convention': False,
@@ -246,6 +256,25 @@ check_result_names = {
     'create_hillshade': '',
     }
 
+tabs_styles = {
+    'height': '44px',
+}
+
+tab_style = {
+    'borderBottom': '1px solid #d6d6d6',
+    'padding': '6px',
+    'fontWeight': 'bold',
+    'color': '#373D42',
+}
+
+tab_selected_style = {
+    'borderTop': '1px solid #d6d6d6',
+    'borderBottom': '1px solid #d6d6d6',
+    'backgroundColor': '#119DFF',
+    'color': 'white',
+    'padding': '6px'
+}
+
 
 def get_check_layer_options():
     options = []
@@ -255,26 +284,8 @@ def get_check_layer_options():
     return options
 
 
-app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
-    html.Div(style={'backgroundColor':'#00ADEF'}, children=[
-        html.Div([
-            html.H6(
-                children='QAQC Dashboard',
-		        style={
-			        'textAlign': 'center',
-			        'color': colors['header']
-		        }),
-            html.H6(
-		        children='Nantucket Islands and Martha\'s Vinyard', 
-		        style={
-		            'textAlign': 'center',
-		            'color': colors['header']
-                }),
-        ], className='four columns'),
-
-        html.Div(style={}, children=[
-
-            html.Div(
+def get_qaqc_settings_tab():
+    return html.Div(
 		        style={
 		            'textAlign': 'Left',
 		            'color': colors['header']
@@ -284,143 +295,141 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
                     html.P(style={'margin': 0}, children=r'{:20s}: {:30s}'.format('Contractor',  'Dewberry')),
                     html.P(style={'margin': 0}, children=r'{:20s}: {:30s}'.format('UTM Zone',  '16'))
             ]),
-        ], className='eight columns'),
-    ], className='row'),
 
-    html.Div([
 
-        html.Div(style={}, children=[
+def get_tile_overview_tab():
+    return html.Div(style={}, children=[
+                html.Div(style={}, children=[
 
-            html.H6(style={'color': colors['text'], 'margin':0}, children='Las Tests'),    
-            dcc.Graph(
-                style={'height': '25vh', 'padding': 0},
-		        id='pre-check-results',
-		        figure={
-			        'data': [
-				        {
-                            'x': [340] * 8,
-                            'y': range(1,9), 
-	                        'type': 'bar', 'name': 'PASSED', 'orientation': 'h'
-                        },
-				        {
-                            'x': [11] * 8, 
-                            'y': range(1,9),
-                            'type': 'bar', 'name': 'FAILED', 'orientation': 'h'
-                        },
-			        ],
-					'layout': {
-                        'margin': {'l': 150, 'r': 5, 't':0},
-						'xaxis': {
-							'title': 'Number of LAS Files'
-							},
-						'yaxis': {
-							'title': None,
-							'titlefont':{
-								'family': 'Courier New, monospace',
-								'size': 20,
-								'color': colors['text']},
-                            'tickvals': range(1, len(checks_to_do.keys())+1),
-                            'ticktext': checks_to_do.keys()
-						},
-						'legend': {'orientation': 'h',
-									'x': 0,
-									'y': 1.25},
-						'barmode': 'stack',
-						'plot_bgcolor': colors['background'],
-						'paper_bgcolor': colors['background'],
-						'font': {
-							'color': colors['text']
-						},
-					}
-				}
-			),
+                    html.H6(
+                        style={'color': colors['text'], 'margin':0}, 
+                        children='Las Tests'),    
+                    dcc.Graph(
+                        style={'height': '25vh', 'padding': 0},
+		                id='pre-check-results',
+		                figure={
+			                'data': [
+				                {
+                                    'x': [340] * 8,
+                                    'y': range(1,9), 
+	                                'type': 'bar', 'name': 'PASSED', 'orientation': 'h'
+                                },
+				                {
+                                    'x': [11] * 8, 
+                                    'y': range(1,9),
+                                    'type': 'bar', 'name': 'FAILED', 'orientation': 'h'
+                                },
+			                ],
+					        'layout': {
+                                'margin': {'l': 150, 'r': 5, 't':0},
+						        'xaxis': {
+							        'title': 'Number of LAS Files'
+							        },
+						        'yaxis': {
+							        'title': None,
+							        'titlefont':{
+								        'family': 'Courier New, monospace',
+								        'size': 20,
+								        'color': colors['text']},
+                                    'tickvals': range(1, len(checks_to_do.keys())+1),
+                                    'ticktext': checks_to_do.keys()
+						        },
+						        'legend': {'orientation': 'h',
+									        'x': 0,
+									        'y': 1.25},
+						        'barmode': 'stack',
+						        'plot_bgcolor': colors['background'],
+						        'paper_bgcolor': colors['background'],
+						        'font': {
+							        'color': colors['text']
+						        },
+					        }
+				        }
+			        ),
 
-            html.H6(style={'color': colors['text'], 'margin':0}, children='Class Counts'),
-            dcc.Graph(
-                style={'height': '20vh'},
-				id='class-code-hist',
-				figure={
-					'data': [
-						{
-                            'values': [2,5,12,43,54,7789,3434,34,343,1],
-                            'labels': ['{}:{}'.format(c, v) for c, v in 
-                                        zip([01,02,03,04,25,26,27,44,56,66], 
-                                            [2,5,12,43,54,7789,3434,34,343,1])],
-                            'textinfo': 'none',
-                            'hoverinfo': 'label+percent+value',
-                            'type': 'pie', 
-                            'name': 'SF'},
-					],
-					'layout': {
-						'title': None,
-                        'margin': {'l': 150, 'r': 25, 't': 0, 'b':25},
-                        'plot_bgcolor': colors['background'],
-						'paper_bgcolor': colors['background'],
-						'font': {
-							'color': colors['text']
-						},
-                        'legend': {
-							'x': -25.0,
-							'y': 1.15},
-					}
-				}
-			),
+                    html.H6(
+                        style={'color': colors['text'], 'margin':0}, 
+                        children='Class Counts'),
+                    dcc.Graph(
+                        style={'height': '20vh'},
+				        id='class-code-hist',
+				        figure={
+					        'data': [
+						        {
+                                    'values': [2,5,12,43,54,7789,3434,34,343,1],
+                                    'labels': ['{}:{}'.format(c, v) for c, v in 
+                                                zip([01,02,03,04,25,26,27,44,56,66], 
+                                                    [2,5,12,43,54,7789,3434,34,343,1])],
+                                    'textinfo': 'none',
+                                    'hoverinfo': 'label+percent+value',
+                                    'type': 'pie', 
+                                    'name': 'SF'},
+					        ],
+					        'layout': {
+						        'title': None,
+                                'margin': {'l': 150, 'r': 25, 't': 0, 'b':25},
+                                'plot_bgcolor': colors['background'],
+						        'paper_bgcolor': colors['background'],
+						        'font': {
+							        'color': colors['text']
+						        },
+                                'legend': {
+							        'x': -25.0,
+							        'y': 1.15},
+					        }
+				        }
+			        ),
 
-            html.H6(style={'color': colors['text'], 'margin':0}, children='Bathymetry Histogram'),
-			dcc.Graph(
-                style={'height': '25vh'},
-				id='depth-hist',
-				figure={
-					'data': [
-						{'x': make_bathy_hist_data()[1], 
-                        'y': make_bathy_hist_data()[0], 'type': 'bar', 'name': 'SF'},
-					],
-					'layout': {
-						'title': None,
-                        'margin': {'l': 150, 'r': 25, 't': 5, 'b':50},
-                        'plot_bgcolor': colors['background'],
-						'paper_bgcolor': colors['background'],
-						'font': {
-							'color': colors['text']
-						},
-                        'xaxis': {
-							'title': 'Depth (m)',
-							'titlefont':{
-								'family': 'Calibri',
-								'size': 20,
-								'color': colors['text']},
-						},
-                        'yaxis': {
-							'title': 'Frquency',
-							'titlefont':{
-								'family': 'Calibri',
-								'size': 20,
-								'color': colors['text']},
-						},
-					}
-				}
-			)
+                    html.H6(
+                        style={'color': colors['text'], 'margin':0}, 
+                        children='Bathymetry Histogram'),
+			        dcc.Graph(
+                        style={'height': '25vh'},
+				        id='depth-hist',
+				        figure={
+					        'data': [
+						        {'x': make_bathy_hist_data()[1], 
+                                'y': make_bathy_hist_data()[0], 'type': 'bar', 'name': 'SF'},
+					        ],
+					        'layout': {
+						        'title': None,
+                                'margin': {'l': 150, 'r': 25, 't': 5, 'b':50},
+                                'plot_bgcolor': colors['background'],
+						        'paper_bgcolor': colors['background'],
+						        'font': {
+							        'color': colors['text']
+						        },
+                                'xaxis': {
+							        'title': 'Depth (m)',
+							        'titlefont':{
+								        'family': 'Calibri',
+								        'size': 20,
+								        'color': colors['text']},
+						        },
+                                'yaxis': {
+							        'title': 'Frquency',
+							        'titlefont':{
+								        'family': 'Calibri',
+								        'size': 20,
+								        'color': colors['text']},
+						        },
+					        }
+				        }
+			        )
 
-		], className='three columns'),
+		        ], className='three columns'),
 
-		html.Div(style={}, children=[
-            
-			html.Div([
+                html.Div([
+                    dcc.Graph(
+					    id='qaqc_map',
+					    style={'height': '90vh'}
+				    ),  
+                ], className='seven columns'),
 
-				dcc.Graph(
-					id='qaqc_map',
-					style={'height': '65vh'}
-				),
+                html.Div(style={'margin-left': '1em'}, children=[
 
-                html.Div(
-				    style={
-					    'float': 'right',
-					    'z-index': 1,
-					    'position': 'absolute',
-				    }, 
-				    children=[
 					    html.P(
-						    style={'color': colors['text'], 'margin':0}, 
+						    style=map_control_label_style, 
 						    children='Test Result:'),
 
                         dcc.Dropdown(
@@ -431,43 +440,158 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
                             value='version'),
 
                         html.P(
-                            style={'color': colors['text'], 'margin':0}, 
-                            children='Map Style'),
+                            style=map_control_label_style, 
+                            children='Map Style:'),
 
                         dcc.Dropdown(
                             style=map_button_style,
+                            id='MapStyleSelector', 
                             options=[
                                 {'label': 'streets', 'value': 'streets'},
+                                {'label': 'dark', 'value': 'dark'},
                                 {'label': 'satellite', 'value': 'satellite'},
                             ],
                             placeholder='Select a Layer',
-                            value='MTL'),
-                ], className='three columns'),
-            ], className='row'),
+                            value='dark'),
 
-            html.Div([
-                generate_table(get_tiles_df())
-            ], className='row'),
+                        html.P(
+                            style=map_control_label_style, 
+                            children='Choose Layers to Display:'),
+
+                        dcc.Checklist(style={
+                            'color': colors['text'],
+                            },
+                            options=[
+                                {'label': 'Contractor tiles', 'value': 'tiles'},
+                                {'label': 'Las Data Extents', 'value': 'las_extents'},
+                                {'label': 'Trajectory', 'value': 'trajectory'}
+                            ],
+                            values=['trajectory']
+                        )
+
+                ], className='two columns'),
+
+        ], className='row')
+
+
+def get_tile_point_cloud_tab():
+    return html.Div([
+                    generate_table(get_tiles_df())
+                ], className='row'),
+
+
+def get_qaqc_result_table_tab():
+    return html.Div([
+                    generate_table(get_tiles_df())
+                ], className='row'),
+
+
+def get_qaqc_log_tab():
+    return html.Div([
+                    generate_table(get_tiles_df())
+                ], className='row'),
+
+
+app.config['suppress_callback_exceptions']=True
+app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
+    html.Div(style={'backgroundColor':'#00ADEF'}, children=[
+        html.Div(style={}, children=[
+            html.P(
+		        children='QAQC: Nantucket Islands and Martha\'s Vinyard', 
+		        style={
+                    'textAlign': 'center',
+		            'color': colors['header'],
+                    'font-size': 16,
+                    'font-weight': 'bold',
+                    'vertical-align': 'middle',
+                    'line-height': '5vh',
+                    'margin': 0,
+                    'padding': 0
+                }),
+        ], className='three columns'),
+
+        html.Div(style={'margin-left': 0}, children=[
+
+            dcc.Tabs(
+                id="tabs-header", 
+                value='tab-1', 
+                style=tabs_styles,
+                children=[
+                    
+                    dcc.Tab(
+                        style=tab_style, 
+                        selected_style=tab_selected_style,
+                        label='QAQC Settings',
+                        value='qaqc_settings-tab'),
+
+                    dcc.Tab(
+                        style=tab_style, 
+                        selected_style=tab_selected_style,
+                        label='Tile Overview',
+                        value='tile-overview-tab'),
+                    
+                    dcc.Tab(
+                        style=tab_style, 
+                        selected_style=tab_selected_style,
+                        label='Tile Point Cloud',
+                        value='tile-point-cloud-tab'),
+
+                    dcc.Tab(
+                        style=tab_style, 
+                        selected_style=tab_selected_style,
+                        label='QAQC Results Table',
+                        value='qaqd-results-table-tab'),
+
+                    dcc.Tab(
+                        style=tab_style, 
+                        selected_style=tab_selected_style,
+                        label='QAQC Log',
+                        value='qaqc-log-tab'),
+
+                ], className='twelve columns'),
 
         ], className='nine columns'),
 
-    ], className='row')
+    ], className='row'),
+
+    html.Div(id='tabs-content')
 
 ], className='no gutters')
 
+
+@app.callback(dash.dependencies.Output('tabs-content', 'children'),
+              [dash.dependencies.Input('tabs-header', 'value')])
+def render_content(tab):
+    if tab == 'qaqc_settings-tab':
+        return get_qaqc_settings_tab()
+    elif tab == 'tile-overview-tab':
+        return get_tile_overview_tab()
+    elif tab == 'tile-point-cloud-tab':
+        return get_tile_point_cloud_tab()
+    elif tab == 'qaqd-results-table-tab':
+        return get_qaqc_result_table_tab()
+    elif tab == 'qaqc-log-tab':
+        return get_qaqc_log_tab()
+
+
 @app.callback(
     dash.dependencies.Output('qaqc_map', 'figure'),
-    [dash.dependencies.Input('CheckResultLayers', 'value')])
-def update_map_layer(selector):
-    print(selector)
-    
-    figure=get_qaqc_map(check_labels[selector], 
-                        check_result_names[selector],
-                        get_contract_tile_csv(),
-                        get_qaqc_results_csv())
+    [dash.dependencies.Input('CheckResultLayers', 'value'),
+     dash.dependencies.Input('MapStyleSelector', 'value')])
+def update_map_layer(input1, input2):
+	print(input1)
+	print(input2)
+	
+	figure=get_qaqc_map(check_labels[input1], 
+						check_result_names[input1],
+						input2,
+						get_contract_tile_csv(),
+						get_qaqc_results_csv())
 
-    return figure
+	return figure
 
 
 if __name__ == '__main__':
+	
 	app.run_server(debug=True)
+
