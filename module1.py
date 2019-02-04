8c651b01e28250727e3e537ec92da41fb3e54745
Nick Forfinski-Sarkozi <nick.forfinski-sarkozi@noaa.gov>
2019-02-04 11:10:51 -0500

    refactored out individual button callbacks with "function instances by closure"

--- module1.py
+++ module1.py
@@ -13,15 +13,6 @@ NORM_FONT = ('Verdanna', 10)
 NORM_FONT_BOLD = ('Verdanna', 10, 'bold')
 SMALL_FONT = ('Verdanna', 8)
 
-section_rows = {
-    'metadata': 0,
-    'files': 1,
-    'dirs': 2,
-    'checks': 3,
-    'surfaces': 4,
-    'run_button': 5,
-    }
-
 
 class QaqcApp(tk.Tk):
 
@@ -35,8 +26,8 @@ class QaqcApp(tk.Tk):
         self.withdraw()
         splash = Splash(self)
 
-        tk.Tk.wm_title(self, 'cBLUE')
-        tk.Tk.iconbitmap(self, 'cBLUE_icon.ico')
+        tk.Tk.wm_title(self, 'QAQC Checker')
+        tk.Tk.iconbitmap(self, 'qaqc.ico')
 
         container = tk.Frame(self)
         container.pack(side='top', fill='both', expand=True)
@@ -78,7 +69,7 @@ class QaqcApp(tk.Tk):
            print("configuration file doesn't exist")
 
     def save_config(self):
-        config = 'cblue_configuration.json'
+        config = 'Z:\qaqc\qaqc_config.json'
         print('saving {}...\n{}'.format(config, self.controller_configuration))
         with open(config, 'w') as fp:
             json.dump(self.controller_configuration, fp)
@@ -86,9 +77,9 @@ class QaqcApp(tk.Tk):
     @staticmethod
     def show_about():
         about = tk.Toplevel()
-        tk.Toplevel.iconbitmap(about, 'cBLUE_icon.ico')
-        about.wm_title('About cBLUE')
-        splash_img = tk.PhotoImage(file='cBLUE_splash.gif')
+        tk.Toplevel.iconbitmap(about, 'qaqc.ico')
+        about.wm_title('About QAQC Checker')
+        splash_img = tk.PhotoImage(file='qaqc.gif')
         label = tk.Label(about, image=splash_img)
         label.pack()
         b1 = ttk.Button(about, text='Ok', command=about.destroy)
@@ -130,9 +121,52 @@ class MainGuiPage(ttk.Frame):
         print self.parent
         print self.controller.controller_configuration
 
+        # set GUI section options
+        self.checks_to_do = {
+            'naming_convention': ['Naming Convention', None],
+            'version': ['Version', None],
+            'pdrf': ['Point Data Record Format (PDRF)', None],
+            'gps_time_type': ['GPS Time Type', None],
+            'hor_datum': ['Horizontal Datum', None],
+            'ver_datum': ['Vertical Datum', None],
+            'point_source_ids': ['Point Source IDs (Flight Line #)', None],
+            'unexpected_classes': ['Unexpected Classes', None],
+            }
+
+        self.metadata = {
+            'project_name': ['Project Name', None],
+            'hor_datum': ['Horizontal Datum', None],
+            'tile_size': ['Tile Size (m)', None],
+            'expected_classes': ['Expected Classes (comma sep.)', None],
+            }
+
+        self.files_to_set = {
+            'contractor_shp': ['Contractor Tile Shapefile', None, 'fpath', None, '.shp'],
+            'dz_classes_template': ['Dz Classes Template', None, 'fpath', None, '.lyr'],
+            'dz_export_settings': ['Dz Export Settings', None, 'fpath', None, '.xml'],
+            'dz_mxd': ['QAQC ArcGIS Map', None, 'fpath', None, '.mxd'],
+            }
+
+        self.dirs_to_set = {
+            'qaqc_dir': ['QAQC Home', None, 'fpath', None],
+            'qaqc_gdb': ['QAQC GeoDatabase', None, 'fpath', None],
+            'las_tile_dir': ['Las Tiles', None, 'fpath', None],
+            'dz_binary_dir': ['Dz Surfaces', None, 'fpath', None],
+            }
+
+        self.section_rows = {
+            'metadata': 0,
+            'files': 1,
+            'dirs': 2,
+            'checks': 3,
+            'surfaces': 4,
+            'run_button': 5,
+            }
+
         #  Build the GUI
         self.control_panel_width = 30
         self.build_gui()
+
 
     def build_gui(self):
 
@@ -147,7 +181,7 @@ class MainGuiPage(ttk.Frame):
         '''Metadata'''
 
         meta_frame = ttk.Frame(self)
-        meta_frame.grid(row=section_rows['metadata'], sticky=tk.NSEW)
+        meta_frame.grid(row=self.section_rows['metadata'], sticky=tk.NSEW)
 
         label = tk.Label(meta_frame, text='Populate Metadata', font=LARGE_FONT_BOLD)
         label.grid(row=0, columnspan=3, pady=(10, 0), sticky=tk.W)
@@ -164,171 +198,112 @@ class MainGuiPage(ttk.Frame):
             print project_ids
             return tuple(project_ids)
 
-        metadata = {
-            'project_name': ['Project Name', None],
-            'hor_datum': ['Horizontal Datum', None],
-            'tile_size': ['Tile Size (m)', None],
-            'expected_classes': ['Expected Classes (comma sep.)', None],
-            }
-
         item = 'project_name'
         row = 1
-        meta_label = tk.Label(meta_frame, text=metadata[item][0])
+        meta_label = tk.Label(meta_frame, text=self.metadata[item][0])
         meta_label.grid(column=0, row=row, sticky=tk.W)
-        metadata[item][1] = tk.StringVar()
-        metadata[item][1].set("(Select Project ID)")
-        proj_down_down = tk.OptionMenu(meta_frame, metadata[item][1], *get_proj_names())
+        self.metadata[item][1] = tk.StringVar()
+        self.metadata[item][1].set("(Select Project ID)")
+        proj_down_down = tk.OptionMenu(meta_frame, self.metadata[item][1], *get_proj_names())
         proj_down_down.grid(column=1, row=row, sticky=tk.EW)
 
         item = 'hor_datum'
         row = 2
-        meta_label = tk.Label(meta_frame, text=metadata[item][0])
+        meta_label = tk.Label(meta_frame, text=self.metadata[item][0])
         meta_label.grid(column=0, row=row, sticky=tk.W)
-        metadata[item][1] = tk.StringVar()
-        metadata[item][1].set("(Select WKT ID)")
-        wkt_ids_drop_down = tk.OptionMenu(meta_frame, metadata[item][1], *get_wkt_ids())
+        self.metadata[item][1] = tk.StringVar()
+        self.metadata[item][1].set("(Select WKT ID)")
+        wkt_ids_drop_down = tk.OptionMenu(meta_frame, self.metadata[item][1], *get_wkt_ids())
         wkt_ids_drop_down.grid(column=1, row=row, sticky=tk.EW)
 
         item = 'expected_classes'
         row = 3
-        meta_label = tk.Label(meta_frame, text=metadata[item][0])
+        meta_label = tk.Label(meta_frame, text=self.metadata[item][0])
         meta_label.grid(column=0, row=row, sticky=tk.W)
-        metadata[item][1] = tk.Entry(meta_frame, width=30)
-        metadata[item][1].grid(column=1, row=row, sticky=tk.EW)
+        self.metadata[item][1] = tk.Entry(meta_frame, width=30)
+        self.metadata[item][1].grid(column=1, row=row, sticky=tk.EW)
 
         item = 'tile_size'
         row = 4
-        meta_label = tk.Label(meta_frame, text=metadata[item][0])
+        meta_label = tk.Label(meta_frame, text=self.metadata[item][0])
         meta_label.grid(column=0, row=row, sticky=tk.W)
-        metadata[item][1] = tk.Entry(meta_frame, width=5)
-        metadata[item][1].grid(column=1, row=row, sticky=tk.EW)
+        self.metadata[item][1] = tk.Entry(meta_frame, width=5)
+        self.metadata[item][1].grid(column=1, row=row, sticky=tk.EW)
 
     def build_files(self):
         '''Files'''
 
         files_frame = ttk.Frame(self)
-        files_frame.grid(row=section_rows['files'], sticky=tk.NSEW)
+        files_frame.grid(row=self.section_rows['files'], sticky=tk.NSEW)
 
         label = tk.Label(files_frame, text='Select Files', font=LARGE_FONT_BOLD)
         label.grid(row=0, columnspan=3, pady=(10, 0), sticky=tk.W)
 
-        def get_file():
-            dir_str = tkFileDialog.askopenfilename()
-            displayed_dir = r'...\{}'.format(dir_str.split('/')[-1])  # tk uses forward slashes
-            print displayed_dir
-            return dir_str, displayed_dir
+        def bind_files_command(f):
+            def func():
+                file_str = tkFileDialog.askopenfilename()
+                displayed_file = r'...\{}'.format(file_str.split('/')[-1])  # tk uses forward slashes
+                self.files_to_set[f][1].configure(text=displayed_file)
+                self.files_to_set[f][2] = file_str 
+            func.__name__ = f
+            return func
 
-        def file0_clicked():
-            user_dir, displayed_dir = get_file()
-            files_to_set['contractor_shp'][1].configure(text=displayed_dir)
-            files_to_set['contractor_shp'][2] = user_dir
-        def file1_clicked():
-            user_dir, displayed_dir = get_file()
-            files_to_set['dz_classes_template'][1].configure(text=displayed_dir)
-            files_to_set['dz_classes_template'][2] = user_dir
-        def file2_clicked():
-            user_dir, displayed_dir = get_file()
-            files_to_set['dz_export_settings'][1].configure(text=displayed_dir)
-            files_to_set['dz_export_settings'][2] = user_dir
-        def file3_clicked():
-            user_dir, displayed_dir = get_file()
-            files_to_set['dz_mxd'][1].configure(text=displayed_dir)
-            files_to_set['dz_mxd'][2] = user_dir
-
-        files_to_set = {
-            'contractor_shp': ['Contractor Tile Shapefile', None, 'fpath',file0_clicked, '.shp'],
-            'dz_classes_template': ['Dz Classes Template', None, 'fpath', file1_clicked, '.lyr'],
-            'dz_export_settings': ['Dz Export Settings', None, 'fpath', file2_clicked, '.xml'],
-            'dz_mxd': ['QAQC ArcGIS Map', None, 'fpath', file3_clicked, '.mxd'],
-            }
-
-        for i, d in enumerate(files_to_set, 1):
-            check_label = tk.Label(files_frame, text=files_to_set[d][0])
+        for i, f in enumerate(self.files_to_set, 1):
+            check_label = tk.Label(files_frame, text=self.files_to_set[f][0])
             check_label.grid(column=0, row=i, sticky=tk.W)
 
-            btn = tk.Button(files_frame, text="...", command=files_to_set[d][3])
+            self.files_to_set[f][1] =tk.Label(files_frame, text='(Select {} file)'.format(self.files_to_set[f][4]))
+            self.files_to_set[f][1].grid(column=2, row=i, sticky=tk.W)
+
+            btn = tk.Button(files_frame, text="...", command=bind_files_command(f))
             btn.grid(column=1, row=i, sticky=tk.W)
 
-            files_to_set[d][1] =tk.Label(files_frame, 
-                                       text='(Select {} file)'.format(files_to_set[d][4]))
-            files_to_set[d][1].grid(column=2, row=i, sticky=tk.W)
     
     def build_dirs(self):
         '''Directories'''
 
         dirs_frame = ttk.Frame(self)
-        dirs_frame.grid(row=section_rows['dirs'], sticky=tk.NSEW)
+        dirs_frame.grid(row=self.section_rows['dirs'], sticky=tk.NSEW)
 
         label = tk.Label(dirs_frame, text='Select Directories', font=LARGE_FONT_BOLD)
         label.grid(row=0, columnspan=3, pady=(10, 0), sticky=tk.W)
 
-        def get_dir():
-            dir_str = tkFileDialog.askdirectory()
-            displayed_dir = '...\{}\\'.format(dir_str.split('/')[-1])  # tk uses forward slashes
-            print displayed_dir
-            return dir_str, displayed_dir
+        def bind_dirs_command(d):
+            def func():
+                dir_str = tkFileDialog.askdirectory()
+                displayed_dir = r'...\{}'.format(dir_str.split('/')[-1])  # tk uses forward slashes
+                self.dirs_to_set[d][1].configure(text=displayed_dir)
+                self.dirs_to_set[d][2] = dir_str 
+            func.__name__ = d
+            return func
 
-        def dir0_clicked():
-            user_dir, displayed_dir = get_dir()
-            dirs_to_set['qaqc_dir'][1].configure(text=displayed_dir)
-            dirs_to_set['qaqc_dir'][2] = user_dir
-        def dir1_clicked():
-            user_dir, displayed_dir = get_dir()
-            dirs_to_set['qaqc_gdb'][1].configure(text=displayed_dir)
-            dirs_to_set['qaqc_gdb'][2] = user_dir
-        def dir2_clicked():
-            user_dir, displayed_dir = get_dir()
-            dirs_to_set['las_tile_dir'][1].configure(text=displayed_dir)
-            dirs_to_set['las_tile_dir'][2] = user_dir
-        def dir3_clicked():
-            user_dir, displayed_dir = get_dir()
-            dirs_to_set['dz_binary_dir'][1].configure(text=displayed_dir)
-            dirs_to_set['dz_binary_dir'][2] = user_dir
-
-        dirs_to_set = {
-            'qaqc_dir': ['QAQC Home', None, 'fpath', dir0_clicked],
-            'qaqc_gdb': ['QAQC GeoDatabase', None, 'fpath', dir1_clicked],
-            'las_tile_dir': ['Las Tiles', None, 'fpath', dir2_clicked],
-            'dz_binary_dir': ['Dz Surfaces', None, 'fpath', dir3_clicked],
-            }
-
-        for i, d in enumerate(dirs_to_set, 1):
-            check_label = tk.Label(dirs_frame, text=dirs_to_set[d][0])
+        for i, d in enumerate(self.dirs_to_set, 1):
+            check_label = tk.Label(dirs_frame, text=self.dirs_to_set[d][0])
             check_label.grid(column=0, row=i, sticky=tk.W)
+            
+            self.dirs_to_set[d][1] = tk.Label(dirs_frame, text="(Select Directory)")
+            self.dirs_to_set[d][1].grid(column=2, row=i, sticky=tk.W)
 
-            btn = tk.Button(dirs_frame, text="...", command=dirs_to_set[d][3])
+            btn = tk.Button(dirs_frame, text="...", command=bind_dirs_command(d))
             btn.grid(column=1, row=i, sticky=tk.W)
 
-            dirs_to_set[d][1] = tk.Label(dirs_frame, text="(Select Directory)")
-            dirs_to_set[d][1].grid(column=2, row=i, sticky=tk.W)
 
     def build_checks(self):
         '''Checks'''
 
         checks_frame = ttk.Frame(self)
-        checks_frame.grid(row=section_rows['checks'], sticky=tk.NSEW)
+        checks_frame.grid(row=self.section_rows['checks'], sticky=tk.NSEW)
 
         label = tk.Label(checks_frame, text='Select Checks', font=LARGE_FONT_BOLD)
         label.grid(row=0, columnspan=3, pady=(10, 0), sticky=tk.W)
 
-        checks_to_do = {
-            'naming_convention': ['Naming Convention', None],
-            'version': ['Version', None],
-            'pdrf': ['Point Data Record Format (PDRF)', None],
-            'gps_time_type': ['GPS Time Type', None],
-            'hor_datum': ['Horizontal Datum', None],
-            'ver_datum': ['Vertical Datum', None],
-            'point_source_ids': ['Point Source IDs (Flight Line #)', None],
-            'unexpected_classes': ['Unexpected Classes', None],
-            }
-
-        for i, c in enumerate(checks_to_do, 1):
-            checks_to_do[c][1] = tk.BooleanVar()
-            checks_to_do[c][1].set(True)
+        for i, c in enumerate(self.checks_to_do, 1):
+            self.checks_to_do[c][1] = tk.BooleanVar()
+            self.checks_to_do[c][1].set(True)
             chk = tk.Checkbutton(
                 checks_frame, 
-                text=checks_to_do[c][0],
-                var=checks_to_do[c][1], 
+                text=self.checks_to_do[c][0],
+                var=self.checks_to_do[c][1], 
                 anchor=tk.W, justify=tk.LEFT)
             chk.grid(column=0, row=i, sticky=tk.W)
 
@@ -336,7 +311,7 @@ class MainGuiPage(ttk.Frame):
         '''Surfaces'''
 
         surf_frame = ttk.Frame(self)
-        surf_frame.grid(row=section_rows['surfaces'], sticky=tk.NSEW)
+        surf_frame.grid(row=self.section_rows['surfaces'], sticky=tk.NSEW)
 
         label = tk.Label(surf_frame, text='Select Surfaces', font=LARGE_FONT_BOLD)
         label.grid(row=0, columnspan=3, pady=(10, 0), sticky=tk.W)
@@ -357,7 +332,7 @@ class MainGuiPage(ttk.Frame):
     def build_run_button(self):
 
         run_frame = ttk.Frame(self)
-        run_frame.grid(row=section_rows['run_button'], sticky=tk.NSEW, pady=(10, 0))
+        run_frame.grid(row=self.section_rows['run_button'], sticky=tk.NSEW, pady=(10, 0))
 
         btn = tk.Button(run_frame, text="Run QAQC Processes", 
              command=self.run_qaqc_process, height=2)
@@ -369,16 +344,16 @@ class MainGuiPage(ttk.Frame):
     def save_settings(self):
         settings = {'checks_to_do': {}}
 
-        for k, v in checks_to_do.iteritems():
+        for k, v in self.checks_to_do.iteritems():
             settings['checks_to_do'].update({k: v[1].get()})
 
-        for k, v in dirs_to_set.iteritems():
+        for k, v in self.dirs_to_set.iteritems():
             settings.update({k: v[2]})
 
-        for k, v in files_to_set.iteritems():
+        for k, v in self.files_to_set.iteritems():
             settings.update({k: v[2]})
 
-        for k, v in metadata.iteritems():
+        for k, v in self.metadata.iteritems():
             settings.update({k: v[1].get()})
 
         for k, v in surfaces_to_make.iteritems():
@@ -393,10 +368,6 @@ class MainGuiPage(ttk.Frame):
         self.save_settings()
         #run_qaqc() #  from qaqc.py
     
-
-
-
-
 
 if __name__ == "__main__":
     app = QaqcApp()
