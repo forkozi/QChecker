5120243d57065c69d278780acf64a88016d52f51
Nick Forfinski-Sarkozi <nick.forfinski-sarkozi@noaa.gov>
2020-10-02 09:34:43 -0400

    added las-version check for classification key

--- hook.py
+++ hook.py
@@ -2,9 +2,11 @@ import os
 import sys
 
 
+pyproj_dir = os.path.dirname(sys.argv[0]) + r'\pyproj'
+gdal_dir = os.path.dirname(sys.argv[0]) + r'\gdal'
 
-os.environ['PROJ_LIB'] = os.path.dirname(sys.argv[0]) + r'\pyproj'
-os.environ['GDAL_DATA'] = os.path.dirname(sys.argv[0]) + r'\gdal'
+print(pyproj_dir)
+print(gdal_dir)
 
-print(os.environ['PROJ_LIB'])
-print(os.environ['GDAL_DATA'])
+os.environ['PROJ_LIB'] = pyproj_dir
+os.environ['GDAL_DATA'] = gdal_dir
